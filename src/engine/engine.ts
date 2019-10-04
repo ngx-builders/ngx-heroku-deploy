4e812efdfe54b680e232419d93458ad1c6a14720
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-10-05 02:58:25 +0530

    added code to deploy

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,8 +1,9 @@
 import { logging } from '@angular-devkit/core';
-import * as fse from 'fs-extra';
 
 import { Schema } from '../deploy/schema';
 const Heroku = require('heroku-client');
+var url = require('url');
+const fetch = require("node-fetch");
 
 // TODO: add your deployment code here!
 export async function run(dir: string, options: Schema, logger: logging.LoggerApi) {
@@ -14,14 +15,39 @@ export async function run(dir: string, options: Schema, logger: logging.LoggerAp
     const result = await heroku.get('/apps');
     const site = result.find((app => app.name === 'ngx-deploy-demo'))
 
-    const slugResult = await heroku.post(`/apps/${site.name}/slugs`,{
-        body: {
-          process_types: { "web": "node-v0.10.20-linux-x64/bin/node index.js" }
-        }
+    const slugResult = await heroku.post(`/apps/${site.name}/slugs`, {
+      body: {
+        process_types: { "web": "node-v0.10.20-linux-x64/bin/node index.js" }
       }
+    }
     );
+
+    // const upload
     // console.log(site);
-    console.log(slugResult.blob.url);
+    console.log(slugResult);
+    const apiUrl = slugResult.blob.url.replace('%3D', '=');
+    console.log(apiUrl);
+
+
+    const response = await fetch(slugResult.blob.url, {
+      method: `${slugResult.blob.method}`, // or 'PUT'
+      // body: JSON.stringify(data), // data can be `string` or {object}!
+      body: '@E:\ngx-deploy-heroku\ngx-deploy-sample\ngx-deploy-sample-0.0.0.tgz',
+      headers: {
+        'Content-Type': ''
+      }
+    });
+    console.log(response);
+    console.log(slugResult.id);
+
+    const release = await heroku.post(`/apps/${site.name}/releases`, {
+      body: {
+        slug: `${slugResult.id}`
+      }
+    });
+
+    console.log(release);
+
   }
   catch (error) {
     logger.error('❌ An error occurred!');
